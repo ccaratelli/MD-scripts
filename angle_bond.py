0ce5095808f448c08f81a107e21a188c22100506
Chiara <chiara.caratelli@gmail.com>
2017-06-20 16:52:40 +0200

    Normalized histogram

--- angle_bond.py
+++ angle_bond.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import numpy as np
 import molmod as md
 import argparse
@@ -8,14 +9,14 @@ from molmod.ic import bond_length
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+#from scipy.stats import norm
 
 def main(file_name):
     xyz_file = XYZFile(file_name)
     outputName = "bond_angles.dat"
     outFilenameGraph = "bond_anglesGraph"
     frames = xyz_file.geometries
-    atomsi = [[170,70,9],[170,70,167],[70,170]]  #add this in the command line
-#    atomsi = [[165,68,5],[165,68,267],[165,68]]  #add this in the command line
+    atomsi = [[170,70,9],[170,70,167],[70,170],[165,68,5],[165,68,267],[165,68]]  #add this in the command line
     atoms = [ list(np.array(a)-1) for a in atomsi ]
 
     bonds  = map(partial(get_bonds, frames), filter(lambda at: len(at) == 2, atoms))
@@ -33,11 +34,13 @@ def plotBonds_Angles(outFilename,allThing,atomsi):
     '''
     (num,steps) = allThing.shape
     for i in range(num)[1:]:
-        hist, bin_edges = np.histogram(allThing[i], bins=50)
+        hist, bin_edges = np.histogram(allThing[i], bins=50,density=True)
         bin_centres = (bin_edges[:-1] + bin_edges[1:])/2
         histogram = np.stack((bin_centres, hist))
         name = convertLabel(atomsi[i-1])
         gaussian, coefficients = fit_gaussian(histogram)
+#        mu, std = norm.fit(allThing[i])
+#        gaussian2 = norm.pdf(bin_centres, mu, std)
         printall = np.stack((bin_centres, hist, gaussian), axis =1 )
         np.savetxt(name+".hist", printall)
         np.savetxt(name+".dat", allThing[i])
@@ -76,29 +79,30 @@ def get_angles(frames,atoms):
         angle[frame] = bend_angle(frames[frame,atoms])[0]
     return angle
 
-def fit_gaussian(data, p0=[3000,2,1]):
+def fit_gaussian(data, p0=[2,0.1]):
     '''
     takes histogram and bins of same shape
     '''
     bins, hist = data[0],data[1] 
-    print hist.shape, bins.shape, data.shape
     coeff, var_matrix = curve_fit(gauss, bins, hist, p0=p0)
     # Get the fitted curve
     hist_fit = gauss(bins, *coeff)
     # get k of the oscillator that generates this distribution and append it to the coefficient list
+    # sqrt(k/2pi kb T)
     temp = 351
     kb = 1.38064852E-23
     JtoHartree = 2.293710449e17
-    k = (temp*kb*JtoHartree)/coeff[2]
+    k = (temp*kb*JtoHartree)/coeff[1]
     allcoeff = np.append(coeff,k)
+    print "k = " k
     return hist_fit, allcoeff
 
 def gauss(x, *p):
     '''
     Define a gaussian function to fit data. A = 1/np.sqrt(2*np.pi*sigma**2)
     '''
-    A, mu, sigma = p
-    return A*np.exp(-(x-mu)**2/(2.*sigma**2))
+    mu, sigma = p
+    return (1/(np.sqrt(2*np.pi)*sigma))*np.exp(-(x-mu)**2/(2.*sigma**2))
     #return (1/np.sqrt(2*np.pi*sigma**2))*numpy.exp(-(x-mu)**2/(2.*sigma**2))
     
 if __name__ == "__main__":
