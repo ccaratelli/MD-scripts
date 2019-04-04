b5e905dbf48dbeee75f5391c902f711921b6dd81
Chiara <chiara.caratelli@gmail.com>
2019-04-04 10:29:26 +0200

    added insert-cell in coord file

--- merge-traj.py
+++ merge-traj.py
@@ -4,7 +4,7 @@ from molmod.io.xyz import XYZFile
 import argparse
 import os
 
-def main(file1_name, file2_name, start_step, end_step, trim):
+def main(file1_name, file2_name, start_step, end_step, trim, unitcell_name):
     if os.path.isfile(file1_name):
         xyz_file1 = XYZFile(file1_name)
         xyz_file3 = XYZFile(file1_name)
@@ -21,14 +21,31 @@ def main(file1_name, file2_name, start_step, end_step, trim):
                 xyz_file3.geometries = np.concatenate((geo1[:min_size:trim], geo2[:min_size:trim]), axis=1)
                 xyz_file3.symbols =  np.concatenate((xyz_file1.symbols, xyz_file2.symbols), axis=0)
                 xyz_file3.write_to_file('coord-ptcv.xyz')
+
+                if unitcell_name != None:
+                    unitcellmerge(xyz_file3, start_step, end_step, trim, unitcell_name)
             else:
                 print('%s does not exist' %(file2_name))
         else:
             xyz_file3.geometries = geo1[::trim]
             xyz_file3.write_to_file('coord-reduced.xyz')
+            if unitcell_name != None:
+                unitcellmerge(xyz_file3, start_step, end_step, trim, unitcell_name)
     else:
         print('%s does not exist' %(file1_name))
 
+def unitcellmerge(xyz_file, start_step, end_step, trim, unitcell_name):
+    '''
+    Takes a md-CELLFILE.cell in cp2k format
+    a xyz trajectory object in molmod 
+    returns comment lines with cell parameters
+    '''
+    with open(unitcell_name,'r') as f:
+        output = f.readlines()[start_step+1:end_step+1:trim]
+    cell = [x.split()[2:11] for x in output]
+    cell2 = [' '.join(x) for x in cell]
+    xyz_file.titles = cell2
+    xyz_file.write_to_file('cell-npt.xyz') #xyz output file
 
 if __name__ == "__main__":
     msg = " angle_bond -i <path/to/trajectory> -tr trim -st start_time -et end_time"
@@ -38,5 +55,6 @@ if __name__ == "__main__":
     parser.add_argument('-st', required=False, default=0, type=int, help='starting time of the simulation (default=0)')
     parser.add_argument('-et', required=False, default=-1, type=int, help='ending time of the simulation')
     parser.add_argument('-tr', required=False, default=1, type=int, help='trim every X steps')
+    parser.add_argument('-uc', required=False, default=None, help='unit cell file')
     args = parser.parse_args()
-    main(args.i, args.p, args.st, args.et, args.tr)
+    main(args.i, args.p, args.st, args.et, args.tr, args.uc)
